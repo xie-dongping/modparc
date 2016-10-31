3fdf6f551ec61a0e322f53f13fd8a230fcfccbdb
谢东平 (XIE Dongping) <dongping.xie.tud@gmail.com>
2016-10-31 17:01:42 +0100

    First Parser Code and Dependencies Analysis

---
+++ modelicaparser/extends.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from funcparserlib.lexer import Token
+from funcparserlib.parser import (some, a, many, skip, finished, maybe,
+                                  with_forward_decls, Parser)
+
+from syntax import keyword, op, token_type
+from modification import class_modification
+
+
+name = (op(".", maybe) + token_type("ident") +
+        maybe(many(op(".") + token_type("ident"))))
+
