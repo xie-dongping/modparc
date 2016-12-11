4ecc69d45864a444b45c77ce81d2e83ba4420fb1
谢东平 (XIE Dongping) <dongping.xie.tud@gmail.com>
2016-12-11 16:42:50 +0100

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='modparc',
-    version='0.1.6',
+    version='0.1.7',
     description="A Modelica parser based on parser generator",
     long_description=readme + '\n\n' + history,
     author="谢东平 Dongping XIE",
