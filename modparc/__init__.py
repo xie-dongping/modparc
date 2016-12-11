2d94377d48131630775405c5e9aed3ea65a07216
谢东平 (XIE Dongping) <dongping.xie.tud@gmail.com>
2016-12-11 21:33:41 +0100

    Bump version: 0.1.7 → 0.2.0

--- modparc/__init__.py
+++ modparc/__init__.py
@@ -24,4 +24,4 @@ sys.setrecursionlimit(10**6)
 
 __author__ = """谢东平 Dongping XIE"""
 __email__ = 'dongping.xie.tud@gmail.com'
-__version__ = '0.1.7'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='modparc',
-    version='0.1.7',
+    version='0.2.0',
     description="A Modelica parser based on parser generator",
     long_description=readme + '\n\n' + history,
     author="谢东平 Dongping XIE",
